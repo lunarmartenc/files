94cacbc1016e628b964ec2b881efda7765a018a9
lunarmartenc <103465519+lunarmartenc@users.noreply.github.com>
2022-04-12 07:14:02 +0600

    Update account.js

--- account.js
+++ account.js
@@ -1,5 +1,5 @@
-var config = {
-  apiKey: "AIzaSyDtjDsi9I3ODEHeVwu-36blfr2eVratWLk",
+ var config = {
+    apiKey: "AIzaSyDtjDsi9I3ODEHeVwu-36blfr2eVratWLk",
     authDomain: "lunarmart-e2410.firebaseapp.com",
     databaseURL: "https://lunarmart-e2410-default-rtdb.firebaseio.com",
     projectId: "lunarmart-e2410",
@@ -7,6 +7,7 @@ var config = {
     messagingSenderId: "168416542459",
     appId: "1:168416542459:web:f87ecc908ab27668b27a5e",
     measurementId: "G-1SR896B8LE"
+        };
 firebase.initializeApp(config);
 
    

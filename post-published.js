b1ef095c3abd703a45362e25df91a3f1bf80ce28
lunarmartenc <103465519+lunarmartenc@users.noreply.github.com>
2022-04-12 17:41:44 +0600

    Add files via upload

--- post-published.js
+++ post-published.js
@@ -1,18 +1,12 @@
-var wurl = window.location.href;
- var wurl2= wurl.replace("https://lunarmart.blogspot.com/p/post-published.html?=", "");
- document.getElementById("Postframe").src = "https://lunarmart.000webhostapp.com/index.php?="+wurl2;
-
-
 var config = {
-    apiKey: "AIzaSyDtjDsi9I3ODEHeVwu-36blfr2eVratWLk",
-    authDomain: "lunarmart-e2410.firebaseapp.com",
-    databaseURL: "https://lunarmart-e2410-default-rtdb.firebaseio.com",
-    projectId: "lunarmart-e2410",
-    storageBucket: "lunarmart-e2410.appspot.com",
-    messagingSenderId: "168416542459",
-    appId: "1:168416542459:web:f87ecc908ab27668b27a5e",
-    measurementId: "G-1SR896B8LE"
-        };
+    apiKey: "AIzaSyCWGbJi9qeD7h6BPhAKBoWupHv21Tge3ck",
+  authDomain: "effort-5efac.firebaseapp.com",
+  databaseURL: "https://effort-5efac.firebaseio.com",
+  projectId: "effort-5efac",
+  storageBucket: "effort-5efac.appspot.com",
+  messagingSenderId: "771931121287",
+  appId: "1:771931121287:web:9fe9abdcab7c369f277a22",
+  measurementId: "G-GLD9CY5VPF"};
 firebase.initializeApp(config);
 
 

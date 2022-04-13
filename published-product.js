b02b2004e32431ae436e3d7f347de78651fae116
lunarmartenc <103465519+lunarmartenc@users.noreply.github.com>
2022-04-13 07:13:35 +0600

    Update published-product.js

--- published-product.js
+++ published-product.js
@@ -55,6 +55,7 @@ firebase.initializeApp(config);
                 var frb = firebase.auth();
                 frb.onAuthStateChanged(user => {
                   if (user) {
+			 
             var Fuid = user.uid;
             var TagFolder = "BE-commerce/User/"+Fuid;
             var RED = firebase.database().ref(TagFolder).child('Position');
@@ -62,7 +63,7 @@ firebase.initializeApp(config);
             var Reddata = (snapshot.val());
 			       
             if(Reddata=="Admin"){
-              
+               document.getElementById("mainWpAdmin").style.display = "block";
             }
             else{
               window.location.href = "cart.html";
@@ -74,6 +75,7 @@ firebase.initializeApp(config);
                     // No user is signed in.
                   }
                 });
+
 
   
   function reply_click(clicked_id)

0a8872ef4340ee3172320a2af250a2f7ab3ae6f5
lunarmartenc <103465519+lunarmartenc@users.noreply.github.com>
2022-04-13 06:34:35 +0600

    Update published-product.js

--- published-product.js
+++ published-product.js
@@ -50,6 +50,31 @@ var config = {
         };
 firebase.initializeApp(config);
   
+
+
+                var frb = firebase.auth();
+                frb.onAuthStateChanged(user => {
+                  if (user) {
+            var Fuid = user.uid;
+            var TagFolder = "BE-commerce/User/"+Fuid;
+            var RED = firebase.database().ref(TagFolder).child('Position');
+            RED.on('value', function(snapshot) {
+            var Reddata = (snapshot.val());
+			       
+            if(Reddata=="Admin"){
+              
+            }
+            else{
+              window.location.href = "cart.html";
+            }
+          }); 
+        }
+        else {
+		window.location.href = "account.html"
+                    // No user is signed in.
+                  }
+                });
+
   
   function reply_click(clicked_id)
     {

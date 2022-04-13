449d0dcad77819e0acdc9389d554fbaaabe1499d
lunarmartenc <103465519+lunarmartenc@users.noreply.github.com>
2022-04-14 03:31:02 +0600

    Update sale-complite.js

--- sale-complite.js
+++ sale-complite.js
@@ -97,9 +97,7 @@ firebase.initializeApp(config);
     
      function Onloddata() {
     
-    var rand=()=>Math.random(0).toString(36).substr(2);
-    var token =(length)=>(rand()+rand()+rand()+rand()).substr(0,length);
-    var randomPath = (token(40));
+    var randomPath = Date.now();
     
     document.getElementById("pathlink2").href = "post-new.html?="+randomPath;
     
